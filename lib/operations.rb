93234b6140dd7ae15992df47445d99ca858f2d71
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-19 18:29:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,9 @@
 def unsafe(speed)
   if speed > 60 
-    return "safe" 
-  else
-    return "unsafe" 
+    return true 
+  elsif speed < 40 
+    return true 
+  else 
+    return false 
   end
 end
